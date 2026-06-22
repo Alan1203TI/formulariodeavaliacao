697eb2fdf32d9cbda16f28db30a9086c9042859f
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-22 08:22:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDErBE9cC4ec7b2DR5b3iwq1bYtuOOgNGs",
   authDomain: "formulario-de-avaliacao.firebaseapp.com",
   projectId: "formulario-de-avaliacao",
